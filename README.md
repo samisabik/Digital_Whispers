8273844334de5dab281e447383dde095432e89dd
Sami Sabik <sabik.sami@gmail.com>
2016-06-15 18:12:07 +0200

    more merge client/server approach

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Digital-Whispers
+
+sudo apt-get update && sudo apt-get upgrade
+sudo apt-get install git build-essential python-dev python-pyaudio
+easy-install pip
+pip install pyaudio
+
+git clone https://github.com/watson-developer-cloud/python-sdk.git
+python setup.py install
+
+git clone https://github.com/popcornmix/omxplayer.git
--- client.py
+++ client.py
@@ -6,7 +6,6 @@ from watson_developer_cloud import TextToSpeechV1,SpeechToTextV1
 from pydub import AudioSegment, playback
 import random
 
-
 ## Watson Congitive API
 text_to_speech = TextToSpeechV1(
     username='96db6c7a-2595-491a-9a62-740dc31e0482',
@@ -16,9 +15,11 @@ speech_to_text = SpeechToTextV1(
     username='a1c7a39e-6618-4274-98f1-6ec5ef7237b8',
     password='pU5vkvlPIpmZ')
 
+STTvoices = ["en-US_AllisonVoice","en-US_LisaVoice","en-GB_KateVoice","en-US_MichaelVoice"]
+
+
 ## utils / debug / setup
-text = "I am the number one, i are bestest in the world"
-voices = ["en-US_AllisonVoice","en-US_LisaVoice","en-GB_KateVoice","en-US_MichaelVoice"]
+text = "This is only a test, please ignore!"
 
 if __name__ == "__main__":
 
@@ -26,7 +27,7 @@ if __name__ == "__main__":
         ## TEXT TO SPEECH API CALL
         with open(join(dirname(__file__), 'output/synthesize.wav'), 'wb') as audio_file:
             start = time.time()
-            audio_file.write(text_to_speech.synthesize(text,voices[random.randrange(0, 4)]))
+            audio_file.write(text_to_speech.synthesize(text,STTvoices[random.randrange(0, 4)],"audio/wav"))
             end = time.time()
             print "[OK] STT %.2f" % (end - start) + "s"
     
@@ -34,14 +35,9 @@ if __name__ == "__main__":
         rec = utility.Recorder(channels=2)
         with rec.open('output/record.wav', 'wb') as recfile:
             recfile.start_recording()
-            
-            ## PLAY TTS Mac
-            TTS_output = AudioSegment.from_wav("output/synthesize.wav")
-            playback.play(TTS_output)
-
             # PLAY STT Linux
 			#os.system('omxplayer -o local output/synthesize.wav')
-           
+            os.system('aplay output/synthesize.wav')
             recfile.stop_recording()
 
     ## SPEECH TO TEXT API CALL
@@ -55,9 +51,3 @@ if __name__ == "__main__":
             print "[INPUT] " + text
             print "[OUTPUT] " + TTS
             text = TTS
-
-# RECORD for TTS
-#    rec = utility.Recorder(channels=2)
-#    with rec.open('output/record.wav', 'wb') as recfile:
-#        recfile.start_recording()
-#        recfile.stop_recording()
