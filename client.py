5f5f0f5b3ef18aff66514f12072eae5ea2d6e434
Sami Sabik <sabik.sami@gmail.com>
2016-06-21 18:59:17 +0200

    cleanup #2

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+
 import json, os, sys, time, utility, socket, random
 from os.path import join, dirname
 from watson_developer_cloud import TextToSpeechV1,SpeechToTextV1
@@ -12,10 +13,11 @@ speech_to_text = SpeechToTextV1(
     username='a1c7a39e-6618-4274-98f1-6ec5ef7237b8',
     password='pU5vkvlPIpmZ')
 
-## SETUP
+## SETUP + TOOLS
 STTvoices = ["en-US_AllisonVoice","en-US_LisaVoice","en-GB_KateVoice","en-US_MichaelVoice"]
 rec = utility.Recorder(channels=2, rate=44100, frames_per_buffer=1024)
 text = "This is only a test, please ignore ! I am going to put some shit here and we will see"
+loopid = 0
 
 ## MAIN
 if __name__ == "__main__":
@@ -24,28 +26,33 @@ if __name__ == "__main__":
 
         ## TEXT TO SPEECH API CALL
         with open(join(dirname(__file__), 'output/synthesize.wav'), 'wb') as audio_file:
-            start = time.time()
+            startTTS = time.time()
             audio_file.write(text_to_speech.synthesize(text,STTvoices[random.randrange(0, 4)],"audio/wav"))
-            end = time.time()
-            print "[OK] STT %.2f" % (end - start) + "s"
+            endTTS = time.time()
 
         # PLAY + RECORD TTS
         with rec.open('output/record.wav', 'wb') as recfile:
             recfile.start_recording()
-            ##PLAY STT Linux
+            ##PLAY STT Linux/Mac
             #os.system('omxplayer -o local output/synthesize.wav')
             #os.system('aplay output/synthesize.wav')
+            # MAC PLAY
             os.system('play output/synthesize.wav')
             recfile.stop_recording()
 
         ## SPEECH TO TEXT API CALL
         with open(join(dirname(__file__), 'output/record.wav'), 'rb') as audio_file:
-            start = time.time()
+            startSTT = time.time()
             result = json.dumps(speech_to_text.recognize(audio_file, content_type='audio/wav'))
             parsed_json = json.loads(result)
-            TTS = parsed_json['results'][0]['alternatives'][0]['transcript']
-            end = time.time()
-            print "[OK] TTS %.2f" % (end - start) + "s"
-            print "[INPUT] " + text
-            print "[OUTPUT] " + TTS
-            text = TTS
+            STT = parsed_json['results'][0]['alternatives'][0]['transcript']
+            endSTT = time.time()
+            print "########## " + loopid + " ##########"
+            print "[*] " + text
+            print "[*] STT delay %.2f" % (endTTS - startTTS) + "s"
+            print "[*] TTS delay %.2f" % (endSTT - startSTT) + "s"
+            print "[*] " + STT
+            print "######################"
+
+            #MAC LOOP
+            text = STT
