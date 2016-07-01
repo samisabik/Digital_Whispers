54c30e69813e0eac84a0871cd381f72bae91bb74
Sami Sabik <sabik.sami@gmail.com>
2016-07-01 19:00:47 +1000

    test

--- TCP/server.py
+++ TCP/server.py
@@ -4,7 +4,7 @@ import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 4371                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
