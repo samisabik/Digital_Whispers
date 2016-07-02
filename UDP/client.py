744e0c7bd294822fa05858da26a4d3d513620180
Sami Sabik <sabik.sami@gmail.com>
2016-07-02 21:26:37 +1000

    fix

--- UDP/client.py
+++ UDP/client.py
@@ -7,7 +7,10 @@ SERVER_IP = "192.168.2.194"
 s = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 s.bind((UDP_IP, UDP_PORT))
+
+
 os.system('clear')
+print "## STARTING ON : " + socket.gethostname()
 
 while True:
 	data, addr = s.recvfrom(1024)
