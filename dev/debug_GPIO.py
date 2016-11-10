217d961409f8466e4d80e0c7f3737e1b02c84634
Sami <sabik.sami@gmail.com>
2016-11-10 19:33:28 +1100

    tweak setup.sh

--- dev/debug_GPIO.py
+++ dev/debug_GPIO.py
@@ -1,5 +1,9 @@
-#https://gist.github.com/lanefu/f16a67195c9fa35c466c6b50cdaeadea
 
+#RAW GPIO
+#echo "198" > /sys/class/gpio/export
+#echo "out" > /sys/class/gpio/gpio198/direction
+#echo "1" > /sys/class/gpio/gpio198/value
+#https://gist.github.com/lanefu/f16a67195c9fa35c466c6b50cdaeadea
 # GPIO port numbers
 import wiringpi
 from time import sleep
--- main/setup/setup.sh
+++ main/setup/setup.sh
@@ -49,3 +49,9 @@ cd ..
 # cleanup
 rm -r python-sdk portaudio pyaudio pa_stable_v19_20140130.tgz sox-14.4.2.tar.gz sox-14.4.2
 
+#Modify .fex to support I2S Audio
+mv custom.fex /boot/custom.fex
+cd /boot
+fex2bin custom.fex > script.bin
+reboot
+
