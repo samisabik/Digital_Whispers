e5147bba4a99c033b24de030b5ce86384a2d2996
Sami <sabik.sami@gmail.com>
2017-01-27 18:17:49 +1100

    modif in setup.sh

--- main/setup/setup.sh
+++ main/setup/setup.sh
@@ -3,46 +3,40 @@ clear
 
 ## Define new HOSTNAME
 hostn=$(cat /etc/hostname)
-echo "Existing hostname is $hostn"
-echo "Enter new hostname: "
+echo "hostname is: $hostn"
+echo "enter new hostname: "
 read newhost
 sed -i "s/$hostn/$newhost/g" /etc/hosts
 sed -i "s/$hostn/$newhost/g" /etc/hostname
-echo "Your new hostname is $newhost"
+echo "new hostname: $newhost"
 
 ## Global apt setup + cleanup
-apt-get update && sudo apt-get -y upgrade
-apt-get install -y build-essential sox libsox-fmt-all python python-dev python-setuptools 
-## libportaudio2 libportaudiocpp0 portaudio19-dev
-apt-get -y remove --auto-remove --purge 'libx11-.*'
-apt-get -y autoremove --purge
-apt-get -y clean
-
-## Install Portaudio
-wget http://portaudio.com/archives/pa_stable_v19_20140130.tgz
-tar xvf pa_stable_v19_20140130.tgz
-cd portaudio
-./configure --without-jack
-make clean && make && make install
-cd ..
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get install -y python-all-dev python-setuptools portaudio19-dev sox libsox-fmt-all libasound-dev
+sudo easy_install pip
 
 ## Install PyAudio
-git clone http://people.csail.mit.edu/hubert/git/pyaudio.git
-cd pyaudio
-python setup.py install
+sudo pip install pyaudio
+
+## Install Portaudio
+wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
+tar xvf pa_stable_v190600_20161030.tgz
+cd portaudio
+./configure --without-jack && make
+sudo make install
 cd ..
 
 ## Install Watson Python SDK
 git clone https://github.com/watson-developer-cloud/python-sdk.git
 cd python-sdk
-python setup.py install
+sudo python setup.py install
 cd ..
 
 ## Setup USB Soundcard and WIFI
-mv asound.conf /etc/asound.conf
+sudo mv asound.conf /etc/asound.conf
 mv wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
 # cleanup and reboot
-rm -r python-sdk portaudio pyaudio pa_stable_v19_20140130.tgz
+sudo rm -r python-sdk portaudio pyaudio pa_stable_v190600_20161030.tgz
 reboot
 
