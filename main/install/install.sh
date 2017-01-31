4f7285c722bd84fe4ba999e1c963f0b81ab49538
Sami <sabik.sami@gmail.com>
2017-02-01 08:26:53 +1100

    big clean up + update setup script

--- main/install/install.sh
+++ main/install/install.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 clear
 
-## Define new HOSTNAME
+# Error out if anything fails.
+set -e
+
+# Make sure script is run as root.
+if [ "$(id -u)" != "0" ]; then
+  echo "Must be run as root with sudo! Try: sudo ./install.sh"
+  exit 1
+fi
+
+echo "Setting new hostname..."
+echo "=========================="
 hostn=$(cat /etc/hostname)
 echo "hostname is: $hostn"
 echo "enter new hostname: "
@@ -10,15 +20,18 @@ sed -i "s/$hostn/$newhost/g" /etc/hosts
 sed -i "s/$hostn/$newhost/g" /etc/hostname
 echo "new hostname: $newhost"
 
-## Global apt setup + cleanup
+echo "Installing dependencies..."
+echo "=========================="
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y python-all-dev python-setuptools portaudio19-dev sox libsox-fmt-all libasound-dev
 sudo easy_install pip
 
-## Install PyAudio
+echo "Installing PyAudio"
+echo "========================="
 sudo pip install pyaudio
 
-## Install Portaudio
+echo "Installing Portaudio"
+echo "========================="
 wget http://www.portaudio.com/archives/pa_stable_v190600_20161030.tgz
 tar xvf pa_stable_v190600_20161030.tgz
 cd portaudio
@@ -26,17 +39,20 @@ cd portaudio
 sudo make install
 cd ..
 
-## Install Watson Python SDK
+echo "Installing IBM Watson SDK"
+echo "========================="
 git clone https://github.com/watson-developer-cloud/python-sdk.git
 cd python-sdk
 sudo python setup.py install
 cd ..
 
-## Setup USB Soundcard and WIFI
+echo "Setting up WIFI and USB Soundcard"
+echo "========================="
 sudo mv asound.conf /etc/asound.conf
 mv wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 
-# cleanup and reboot
+echo "Clean up and reboot"
+echo "========================="
 sudo rm -r python-sdk portaudio pa_stable_v190600_20161030.tgz
 reboot
 
