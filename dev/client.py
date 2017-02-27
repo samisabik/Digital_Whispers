cf9767b9aea9b1c43e42155dec5317e504af225d
Sami <sabik.sami@gmail.com>
2017-02-27 22:27:20 +1100

    V2 work

--- dev/client.py
+++ dev/client.py
@@ -1,4 +1,16 @@
-import socket,time,os,datetime,sys
+import socket,time,os,datetime,sys,random
+from watson_developer_cloud import TextToSpeechV1,SpeechToTextV1
+
+## IBM Watson API
+text_to_speech = TextToSpeechV1(
+    username='96db6c7a-2595-491a-9a62-740dc31e0482',
+    password='azDpe42DlQ5C')
+
+speech_to_text = SpeechToTextV1(
+    username='a1c7a39e-6618-4274-98f1-6ec5ef7237b8',
+    password='pU5vkvlPIpmZ')
+
+TTSvoices = ["en-US_AllisonVoice","en-US_LisaVoice","en-GB_KateVoice","en-US_MichaelVoice"]
 
 UDP_HOST = ""
 UDP_PORT = 2222
@@ -28,6 +40,15 @@ while True:
 	data, addr = s.recvfrom(1024)
 
 	print data
+
+	## TEXT TO SPEECH API CALL
+        with open(join(dirname(__file__), 'output/synthesize.wav'), 'wb') as audio_file:
+            
+            startTTS = time.time()
+            audio_file.write(text_to_speech.synthesize(data,TTSvoices[random.randrange(0, 4)],"audio/wav"))
+            endTTS = time.time()
+    os.system('play -q --ignore-length output/synthesize.wav')
+
 
 	# if (data == "start_L"):
 	# 	ts = datetime.datetime.fromtimestamp(time.time()).strftime('[%H:%M:%S] ')
