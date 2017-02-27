c80b5592fbdbd17174804de71d66a66ab1c51631
Sami <sabik.sami@gmail.com>
2017-02-27 22:30:23 +1100

    V2 work

--- dev/client.py
+++ dev/client.py
@@ -36,21 +36,15 @@ print "## STARTING ON : " + socket.gethostname()
 print ""
 
 while True:
-
 	data, addr = s.recvfrom(1024)
-
 	print data
-
 	## TEXT TO SPEECH API CALL
     with open(join(dirname(__file__), 'output/synthesize.wav'), 'wb') as audio_file: 
     	audio_file.write(text_to_speech.synthesize(data,TTSvoices[random.randrange(0, 4)],"audio/wav"))
     os.system('play -q --ignore-length output/synthesize.wav')
-
-
 	# if (data == "start_L"):
 	# 	ts = datetime.datetime.fromtimestamp(time.time()).strftime('[%H:%M:%S] ')
 	# 	print ts + "START LISTEN"
-	
 	# if (data == "stop_L"):
 	# 	ts = datetime.datetime.fromtimestamp(time.time()).strftime('[%H:%M:%S] ')
 	# 	print ts + "STOP LISTEN"		
@@ -62,6 +56,3 @@ while True:
 	# 	s.sendto('stop_T', (SERVER_IP,UDP_PORT))
 	# 	ts = datetime.datetime.fromtimestamp(time.time()).strftime('[%H:%M:%S] ')
 	# 	print ts + "STOP TALK"
-
-
-
