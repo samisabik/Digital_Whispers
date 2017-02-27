59fdb7424f111f8db017fa2f91feb4c365db9d66
Sami <sabik.sami@gmail.com>
2017-02-27 22:11:48 +1100

    modif V2

--- main/whisper_V2.py
+++ main/whisper_V2.py
@@ -22,92 +22,9 @@ CHUNK = 4096
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-THRESHOLD = 1500  
+THRESHOLD = audio_int() + 300  
 SILENCE_LIMIT = 2 
 PREV_AUDIO = 0.5  
-
-def audio_int(num_samples=50):
-
-    print "Getting intensity values from mic."
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    values = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4))) 
-              for x in range(num_samples)] 
-    values = sorted(values, reverse=True)
-    r = sum(values[:int(num_samples * 0.2)]) / int(num_samples * 0.2)
-    print " Finished "
-    print " Average audio intensity is ", r
-    stream.close()
-    p.terminate()
-    return r
-
-
-def listen_for_speech(threshold=THRESHOLD, num_phrases=1):
-
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=FORMAT,
-                    channels=CHANNELS,
-                    rate=RATE,
-                    input=True,
-                    frames_per_buffer=CHUNK)
-
-    print "* Listening mic. "
-    audio2send = []
-    cur_data = ''  # current chunk  of audio data
-    rel = RATE/CHUNK
-    slid_win = deque(maxlen=SILENCE_LIMIT * rel)
-    #Prepend audio from 0.5 seconds before noise was detected
-    prev_audio = deque(maxlen=PREV_AUDIO * rel) 
-    started = False
-    n = num_phrases
-    response = []
-
-    while (num_phrases == -1 or n > 0):
-        cur_data = stream.read(CHUNK)
-        slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
-        #print slid_win[-1]
-        if(sum([x > THRESHOLD for x in slid_win]) > 0):
-            if(not started):
-                print "Starting record of phrase"
-                started = True
-            audio2send.append(cur_data)
-        elif (started is True):
-            print "Finished"
-            # The limit was reached, finish capture and deliver.
-            filename = save_speech(list(prev_audio) + audio2send, p)
-            # Reset all
-            started = False
-            slid_win = deque(maxlen=SILENCE_LIMIT * rel)
-            prev_audio = deque(maxlen=0.5 * rel) 
-            audio2send = []
-            n -= 1
-            print "Listening ..."
-        else:
-            prev_audio.append(cur_data)
-
-    print "* Done recording"
-    stream.close()
-    p.terminate()
-
-    return response
-
-def save_speech(data, p):
-
-    data = ''.join(data)
-    wf = wave.open('record.wav', 'wb')
-    wf.setnchannels(1)
-    wf.setsampwidth(p.get_sample_size(pyaudio.paInt16))
-    wf.setframerate(44100) 
-    wf.writeframes(data)
-    wf.close()
-    return 'record.wav'
 
 ## SETUP + TOOLS
 audio_int()
