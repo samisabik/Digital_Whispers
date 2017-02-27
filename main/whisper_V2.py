5ce6587a8b0b8d337ac05d1c6bc3d7d8ca44e40b
Sami <sabik.sami@gmail.com>
2017-02-27 21:30:12 +1100

    start re-work V2

--- main/whisper_V2.py
+++ main/whisper_V2.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
-import json, os, sys, time, utils
+import json, os, sys, time, random, utils
 from os.path import join, dirname
-from watson_developer_cloud import SpeechToTextV1
+from watson_developer_cloud import TextToSpeechV1,SpeechToTextV1
 import audioop
 from collections import deque
+import math
 import pyaudio, wave
+import RPi.GPIO as GPIO
+
+## Watson Congitive API
+text_to_speech = TextToSpeechV1(
+    username='96db6c7a-2595-491a-9a62-740dc31e0482',
+    password='azDpe42DlQ5C')
 
 speech_to_text = SpeechToTextV1(
     username='a1c7a39e-6618-4274-98f1-6ec5ef7237b8',
@@ -18,6 +25,28 @@ RATE = 44100
 THRESHOLD = 1500  
 SILENCE_LIMIT = 2 
 PREV_AUDIO = 0.5  
+
+def audio_int(num_samples=50):
+
+    print "Getting intensity values from mic."
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+    values = [math.sqrt(abs(audioop.avg(stream.read(CHUNK), 4))) 
+              for x in range(num_samples)] 
+    values = sorted(values, reverse=True)
+    r = sum(values[:int(num_samples * 0.2)]) / int(num_samples * 0.2)
+    print " Finished "
+    print " Average audio intensity is ", r
+    stream.close()
+    p.terminate()
+    return r
+
 
 def listen_for_speech(threshold=THRESHOLD, num_phrases=1):
 
@@ -81,6 +110,7 @@ def save_speech(data, p):
     return 'record.wav'
 
 ## SETUP + TOOLS
+audio_int()
 rec = utils.Recorder(channels=1)
 listen_for_speech()
 with open(join(dirname(__file__), 'record.wav'), 'rb') as audio_file:
