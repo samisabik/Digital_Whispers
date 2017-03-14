e20c2b0eb00a8a50dd318b10a27056fa51861ec4
Sami <sabik.sami@gmail.com>
2017-03-14 19:09:47 +1100

    clean up console

--- tools/updateall.sh
+++ tools/updateall.sh
@@ -2,7 +2,7 @@
 echo "Updating master"
 ssh pi@whisper_master 'cd Digital_Whispers && git fetch origin master && git reset --hard origin/master && sudo systemctl restart whisper_server'
 
-for i in `seq 0 6`; do
-    echo "Updating whisper_$i"
-    ssh pi@whisper_$i 'cd Digital_Whispers && git fetch origin master && git reset --hard origin/master && sudo systemctl restart whisper_client'
+#for i in `seq 0 6`; do
+#    echo "Updating whisper_$i"
+#    ssh pi@whisper_$i 'cd Digital_Whispers && git fetch origin master && git reset --hard origin/master && sudo systemctl restart whisper_client'
 done
